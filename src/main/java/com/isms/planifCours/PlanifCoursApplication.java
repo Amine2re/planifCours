89827140478a6f67e062cabfe4439aed2d980650
Mouhamadoul Amine Toure <amine.toure@m2t.biz>
2023-05-24 13:53:57 +0000

    first step

---
+++ src/main/java/com/isms/planifCours/PlanifCoursApplication.java
@@ -0,0 +1,13 @@
+package com.isms.planifCours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanifCoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanifCoursApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isms/planifCours/PlanifCoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isms.planifCours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanifCoursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
